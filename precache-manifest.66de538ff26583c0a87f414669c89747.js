b0cace2bd9a88f0307a35f3e0de09787c8bbbce1
Johan Guzman <jguzm022@ucr.edu>
2020-10-07 10:02:17 -0700

    Updates

---
+++ precache-manifest.66de538ff26583c0a87f414669c89747.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77df807b4029501e2ae56cff4213411",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "684f218be386e1f0bea8",
+    "url": "/Portfolio/static/css/main.07e7cd83.chunk.css"
+  },
+  {
+    "revision": "5359985326eeb3926929",
+    "url": "/Portfolio/static/js/2.d9afbd1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.d9afbd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684f218be386e1f0bea8",
+    "url": "/Portfolio/static/js/main.6d9adc9d.chunk.js"
+  },
+  {
+    "revision": "5e8a2624bf0c1e9aa099",
+    "url": "/Portfolio/static/js/runtime-main.d4934111.js"
+  },
+  {
+    "revision": "e1be13810c83e33a72e83d1767ce1e6d",
+    "url": "/Portfolio/static/media/Magnitie1.e1be1381.jpg"
+  },
+  {
+    "revision": "88fbc6f4f3a9260dbe4ab967919cea65",
+    "url": "/Portfolio/static/media/Magnitie2.88fbc6f4.jpg"
+  },
+  {
+    "revision": "8ca0b057c422f8a8fed7bcf1803a87d4",
+    "url": "/Portfolio/static/media/Magnitie3.8ca0b057.jpg"
+  },
+  {
+    "revision": "bc9ae60905fc53de0ad7b36ec7bbb047",
+    "url": "/Portfolio/static/media/Magnitie4.bc9ae609.jpg"
+  },
+  {
+    "revision": "5eed349e2ff65fa9b97a00a57410e0d7",
+    "url": "/Portfolio/static/media/Magnitie5.5eed349e.jpg"
+  },
+  {
+    "revision": "fff3b42b52e4e28d44a3fbd38f947172",
+    "url": "/Portfolio/static/media/MoodMixer1.fff3b42b.png"
+  },
+  {
+    "revision": "b1a0c4bdbafa6530ea498520bdc169e6",
+    "url": "/Portfolio/static/media/MoodMixer2.b1a0c4bd.png"
+  },
+  {
+    "revision": "6e07ef34e60499838dfc0e8e0230cf4b",
+    "url": "/Portfolio/static/media/MoodMixer3.6e07ef34.png"
+  },
+  {
+    "revision": "e2b5f2abfdfdee2477063874d5ee6870",
+    "url": "/Portfolio/static/media/TIMI1.e2b5f2ab.png"
+  },
+  {
+    "revision": "2a54e8e7a8553aa5809991dff16f96c9",
+    "url": "/Portfolio/static/media/TIMI2.2a54e8e7.png"
+  },
+  {
+    "revision": "ea1aa96ba352e0636e2ab6415f22dc4e",
+    "url": "/Portfolio/static/media/ThErapyScreening.ea1aa96b.jpg"
+  }
+]);
